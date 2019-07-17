020cbb8c5c166dd4fa196e71dbdd4b0344a42c5d
srkravikr <ravikumar98685225@gmail.com>
2019-07-17 13:08:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Hi! Hello 
+          Hi! Hello  from SRAVI
         </p>
       </div>
     );
