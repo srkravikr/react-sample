9b85e063190d579c77d0e9ce9f3468172ee9de85
srkravikr <ravikumar98685225@gmail.com>
2019-07-24 10:47:48 +0530

    2 files modified

--- Components/Pages/Home.jsx
+++ Components/Pages/Home.jsx
@@ -1,30 +1,35 @@
 import React, {Fragment} from 'react';
+import {Carousel} from 'react-bootstrap';
 
 
 const Home = (props) => {
   const HomePageHeroSlides = [
-    {imge:"https://via.placeholder.com/1920X750"},
-    {imge:"https://via.placeholder.com/1920X800"},
-    {imge:"https://via.placeholder.com/1920X750"}
+    {img:"https://via.placeholder.com/1920X750"},
+    {img:"https://via.placeholder.com/1920X800"},
+    {img:"https://via.placeholder.com/1920X750"}
   ]
 
-  const slides=HomePageHeroSlides.map((item) => {
-    console.log(item);
-          <img src={item.imge} alt="/dsfg" />
-    })
+  const slides=HomePageHeroSlides.map((item, index) => {
+      return(
+
+        <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={item.img}
+          alt="First slide"
+        />
+      </Carousel.Item>
+      )
+    });
   return (
     
     <Fragment>
-    <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-      <div className="carousel-inner">
-      {console.log(slides)}
-      {slides}
-    </div>
-  </div>
-
-    <div className="container">
-      <h1>Home Works </h1>
-    </div>
+    
+      <div className="container">
+        <Carousel>
+          {slides}
+        </Carousel>
+      </div>
     </Fragment>
     )
 }
